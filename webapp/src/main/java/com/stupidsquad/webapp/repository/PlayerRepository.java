092765626996a88252937d4f8b4bf24a8b1d9cdd
Bisonpasfute <thibault.cothenet@orange.fr>
2025-03-19 15:15:28 +0100

    Rename PlayerRepository + Add comments

--- webapp/src/main/java/com/stupidsquad/webapp/repository/PlayerRepository.java
+++ webapp/src/main/java/com/stupidsquad/webapp/repository/PlayerRepository.java
@@ -3,6 +3,6 @@ package com.stupidsquad.webapp.repository;
 import com.stupidsquad.webapp.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Player, Long> {
+public interface PlayerRepository extends JpaRepository<Player, Long> {
 
 }
--- webapp/src/main/java/com/stupidsquad/webapp/service/AttendanceService.java
+++ webapp/src/main/java/com/stupidsquad/webapp/service/AttendanceService.java
@@ -2,7 +2,7 @@ package com.stupidsquad.webapp.service;
 
 import com.stupidsquad.webapp.dto.*;
 import com.stupidsquad.webapp.model.Player;
-import com.stupidsquad.webapp.repository.UserRepository;
+import com.stupidsquad.webapp.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +15,7 @@ import java.util.Map;
 @Service
 public class AttendanceService {
     @Autowired
-    UserRepository userRepository;
+    PlayerRepository playerRepository;
 
     @Autowired
     RaidHelperService raidHelperService;
@@ -24,21 +24,21 @@ public class AttendanceService {
         Map<Long, AttendanceDTO> attendanceDTOMap = new HashMap<>();
         List<AttendanceStatisticsDTO> statisticsDTOList = new ArrayList<>();
 
-        // Addition of new players
+        // Addition of missing players (players who signed up but don't exist in the player table)
         if (eventsDTO.getPostedEvents() != null) {
             eventsDTO.getPostedEvents().forEach(postedEventDTO -> {
                 if (postedEventDTO.getSignUps() != null) {
                     postedEventDTO.getSignUps().forEach(signUpDTO -> {
                         long userId = Long.parseLong(signUpDTO.getUserId());
-                        if (userRepository.findById(userId).isEmpty()) {
-                            userRepository.save(new Player(userId, signUpDTO.getName(), new Date(System.currentTimeMillis())));
+                        if (playerRepository.findById(userId).isEmpty()) {
+                            playerRepository.save(new Player(userId, signUpDTO.getName(), new Date(System.currentTimeMillis())));
                         }
                     });
                 }
             });
         }
 
-        List<Player> listPlayer = userRepository.findAll();
+        List<Player> listPlayer = playerRepository.findAll();
         listPlayer.forEach(player -> attendanceDTOMap.put(player.getId(), new AttendanceDTO(player)));
 
         if (eventsDTO.getPostedEvents() != null) {
@@ -46,20 +46,21 @@ public class AttendanceService {
                 RaidPlanDTO raidPlanDTO = raidHelperService.getRaidPlan(Long.parseLong(postedEventDTO.getId()));
                 if (raidPlanDTO != null) {
                     if (postedEventDTO.getSignUps() != null) {
+                        // For each sign-up, increase their sign-up count
                         postedEventDTO.getSignUps().forEach(signUpDTO -> {
                             long userId = Long.parseLong(signUpDTO.getUserId());
-                            //The player has signed up for that event
                             attendanceDTOMap.get(userId).setSignUpCount(attendanceDTOMap.get(userId).getSignUpCount() + 1);
                         });
                     }
+                    // For each player-up, if the player arrived before the date of the event, increase their total event count
                     attendanceDTOMap.forEach((aLong, attendanceDTO) -> {
-                        //If the player arrived before the date of the event, they should have signed up
                         if (postedEventDTO.getStartTime() * 1000L >= attendanceDTO.getArrivalDate().getTime()) {
                             attendanceDTO.setTotalEventCount(attendanceDTO.getTotalEventCount() + 1);
                         }
                     });
-                    if (postedEventDTO.getId() != null && !postedEventDTO.getId().isEmpty()) {
 
+                    // Find players who are signed up but not rostered (bench players) and update their bench count
+                    if (postedEventDTO.getId() != null && !postedEventDTO.getId().isEmpty()) {
                         List<Long> rosteredPlayers = raidPlanDTO.getRaidDropDTO()
                                 .stream()
                                 .map(raidDropDTO -> Long.parseLong(raidDropDTO.getUserid()))
@@ -76,6 +77,7 @@ public class AttendanceService {
             });
         }
 
+        // Calculate the final attendance statistics for each player
         attendanceDTOMap.forEach((aLong, attendanceDTO) -> {
             float benchPercentage = ((float) attendanceDTO.getBenchCount() / (float) attendanceDTO.getTotalEventCount()) * 100F;
             float signUpPercentage = ((float) attendanceDTO.getSignUpCount() / (float) attendanceDTO.getTotalEventCount()) * 100F;
